a134a6b970065fcc6c6d8f58719d400f5360338a
inmedia2019 <54426023+inmedia2019@users.noreply.github.com>
2023-07-27 19:47:13 +0800

    6

---
+++ web/App_Code/jscode2sessionModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// jscode2sessionModel 的摘要说明
+/// </summary>
+public class jscode2sessionModel
+{
+    public jscode2sessionModel()
+    {     
+    }
+
+    public string openid { get; set; }
+    public string session_key { get; set; }
+    public string unionid { get; set; }
+    public int errcode { get; set; }
+    public string errmsg { get; set; }
+}
